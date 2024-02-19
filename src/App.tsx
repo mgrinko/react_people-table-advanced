6770729cecc1f98e94190a0bd1afb6e347f7605e
Misha Grinko <glukym@gmail.com>
2024-02-19 09:44:02 +0200

    add page header

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,8 @@ export const App = () => {
     <div data-cy="app">
       <Navbar />
 
+      <header>Page Header</header>
+
       <div className="section">
         <div className="container">
           <h1 className="title">Home Page</h1>
